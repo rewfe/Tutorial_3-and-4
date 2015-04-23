6e07a058487e4f66cdbb47989c067b78c39febad
rewfe <qrewfe@yandex.ru>
2015-04-24 01:49:10 +0300

    Initial Commit

---
+++ Tutorial_3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tutorial_3
+//
+//  Created by Admin on 24.04.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
